7b437cd930d956a2c160e38ed7e812f9c747e495
Nat Welch <nat@natwelch.com>
2021-01-22 18:29:26 +0000

    plaid

---
+++ plaid.go
@@ -0,0 +1,24 @@
+package lunchmoney
+
+// PlaidAccountResponse is a single plaid account response.
+type PlaidAccountResponse struct {
+	Error         string          `json:"error"`
+	PlaidAccounts []*PlaidAccount `json:"plaid_accounts"`
+}
+
+// PlaidAccount is a single LM Plaid account.
+type PlaidAccount struct {
+	ID                int64     `json:"id"`
+	DateLinked        time.Time `json:"date_linked"`
+	Name              string    `json:"name"`
+	Type              string    `json:"type"`
+	Subtype           string    `json:"subtype"`
+	Mask              string    `json:"mask"`
+	InstitutionName   string    `json:"institution_name"`
+	Status            string    `json:"status"`
+	LastImport        time.Time `json:"last_import"`
+	Balance           string    `json:"balance"`
+	Currency          string    `json:"currency"`
+	BalanceLastUpdate time.Time `json:"balance_last_update"`
+	Limit             int64     `json:"limit"`
+}

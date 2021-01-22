329225cadff8a147651f692aa481bb69536cc885
Nat Welch <nat@natwelch.com>
2021-01-22 17:04:56 +0000

    flesh out client a bit

--- client.go
+++ client.go
@@ -3,7 +3,9 @@ package lunchmoney
 import (
 	"context"
 	"fmt"
+	"io"
 	"net/http"
+	"net/url"
 )
 
 type addAuthHeaderTransport struct {
@@ -22,14 +24,51 @@ func (adt *addAuthHeaderTransport) RoundTrip(req *http.Request) (*http.Response,
 	return adt.T.RoundTrip(req)
 }
 
+// Client holds our base configuration for our LunchMoney client.
 type Client struct {
-	hc *http.Client
+	Http *http.Client
+	Base *url.URL
 }
 
-func NewClient(ctx context.Context, apikey string) (*Client, error) {
+// NewClient creates a new client with the specified API keu
+func NewClient(apikey string) (*Client, error) {
+	base, err := url.Parse("https://dev.lunchmoney.app/v1/")
+	if err != nil {
+		return nil, fmt.Errorf("invalid base URI: %w", err)
+	}
+
 	return &Client{
 		hc: &http.Client{
 			Transport: &addHeaderTransport{T: http.DefaultTransport, Key: apikey},
 		},
+		Base: base,
 	}, nil
+}
+
+// Get makes a request using the client to the path specified with the
+// key/value pairs specified in options. It returns the body of the response or
+// an error.
+func (c *Client) Get(ctx context.Context, path string, options map[string]string) (io.Reader, error) {
+	u, err := url.Parse(c.Base.String() + path)
+	if err != nil {
+		return nil, fmt.Errorf("bad path: %w", err)
+	}
+
+	query := u.Query()
+	for k, v := range options {
+		query.Set(k, v)
+	}
+	u.RawQuery = query.Encode()
+
+	req := &http.Request{
+		Method: http.MethodGet,
+		URL:    u,
+	}
+
+	resp, err := c.Http.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("request (%+v) failed: %w", req, err)
+	}
+
+	return resp.Body, nil
 }

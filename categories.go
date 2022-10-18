e35d3b99af810d05159074e071b9f53525f5461d
Kishore Valluri <SriRanga_Valluri@edgeverve.com>
2022-10-18 17:07:52 +0530

    added categories api for fetching

--- categories.go
+++ categories.go
@@ -1,6 +1,15 @@
 package lunchmoney
 
-import "time"
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/go-playground/validator/v10"
+)
 
 // CategoriesResponse is the response we get from requesting categories.
 type CategoriesResponse struct {
@@ -20,4 +29,64 @@ type Category struct {
 	CreatedAt         time.Time `json:"created_at"`
 	IsGroup           bool      `json:"is_group"`
 	GroupID           int64     `json:"group_id"`
+}
+
+// GetCategories returns categories 
+func (c *Client) GetCategories(ctx context.Context) ([]*Category, error) {
+	validate := validator.New()
+	options := map[string]string{}
+	body, err := c.Get(ctx, "/v1/categories", options)
+	if err != nil {
+		return nil, fmt.Errorf("get categories: %w", err)
+	}
+
+	var resp *CategoriesResponse
+	var bodyCopy bytes.Buffer
+	tee := io.TeeReader(body, &bodyCopy)
+	if err := json.NewDecoder(tee).Decode(&resp); err != nil {
+		return nil, fmt.Errorf("decode response: %w", err)
+	}
+
+	for _, b := range resp.Categories {
+		if err := validate.StructCtx(ctx, b); err != nil {
+			switch v := err.(type) {
+			case validator.ValidationErrors:
+				return nil, fmt.Errorf("validating response: %s", v.Error())
+			case *validator.InvalidValidationError:
+				return nil, fmt.Errorf("validating response (InvalidValidation): %s", v.Error())
+			default:
+				return nil, fmt.Errorf("validating response (%T): %w", err, v)
+			}
+		}
+	}
+	return resp.Categories, nil
+}
+
+func (c *Client) GetCategory( ctx context.Context, id int64) (*Category, error)  {
+	options := map[string]string{}
+	body, err := c.Get(ctx, fmt.Sprintf("/v1/categories/%d", id), options)
+	if err != nil {
+		return nil, fmt.Errorf("error getting category: %w", err)
+	}
+
+	var resp *Category
+	var bodyCopy bytes.Buffer
+	tee := io.TeeReader(body, &bodyCopy)
+	if err := json.NewDecoder(tee).Decode(&resp); err != nil {
+		return nil, fmt.Errorf("error getting category: %w", err)
+	}
+
+	validate := validator.New()
+	if err := validate.StructCtx(ctx, resp); err != nil {
+		switch v := err.(type) {
+		case validator.ValidationErrors:
+			return nil, fmt.Errorf("validating response: %s", v.Error())
+		case *validator.InvalidValidationError:
+			return nil, fmt.Errorf("validating response (InvalidValidation): %s", v.Error())
+		default:
+			return nil, fmt.Errorf("validating response (%T): %w", err, v)
+		}
+	}
+
+	return resp, nil
 }

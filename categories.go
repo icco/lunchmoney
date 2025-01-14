79605c9438c8cdeda6620806c3b867a264de6a4b
Ryan Sheppard <rshep3087@gmail.com>
2025-01-14 13:40:34 -0800

    Add GetCategories function and update requests
    The motivation for the change is to provide a new feature that allows users to retrieve a list of all categories associated with their account in alphabetical order. This enhances the application's functionality by enabling better organization and access to category data.
    
    The change differs from the previous implementation by introducing a new function, `GetCategories`, in the `categories.go` file. This function uses the `Get` method to fetch category data from the API and decodes the JSON response into a `CategoriesResponse` struct. Additionally, the `requests.http` file is updated to include a new HTTP GET request for fetching categories, which complements the new function.

--- categories.go
+++ categories.go
@@ -1,6 +1,11 @@
 package lunchmoney
 
-import "time"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+)
 
 // CategoriesResponse is the response we get from requesting categories.
 type CategoriesResponse struct {
@@ -20,4 +25,20 @@ type Category struct {
 	CreatedAt         time.Time `json:"created_at"`
 	IsGroup           bool      `json:"is_group"`
 	GroupID           int64     `json:"group_id"`
+}
+
+// GetCategories to get a flattened list of all categories in
+// alphabetical order associated with the user's account.
+func (c *Client) GetCategories(ctx context.Context) ([]*Category, error) {
+	body, err := c.Get(ctx, "/v1/categories", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp := &CategoriesResponse{}
+	if err := json.NewDecoder(body).Decode(resp); err != nil {
+		return nil, fmt.Errorf("decode response: %w", err)
+	}
+
+	return resp.Categories, nil
 }
